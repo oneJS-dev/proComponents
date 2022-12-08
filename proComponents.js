bd377bd92060ee2f75b5bb1b07d5d6ccce145a61
oneJS-dev <onejs.dev@gmail.com>
2022-12-08 22:18:35 +0100

    automatic update

--- proComponents.js
+++ proComponents.js
@@ -409,7 +409,7 @@ export const CodeDisplay = ({type = 'both', device = 'web', template = 'react', 
             dependencies: {
                 react: "18.2.0",
                 "react-dom": "18.2.0",
-                "@onejs-dev/components": '0.0.24',
+                "@onejs-dev/components": '0.0.25',
                 ...dependencies
             },
             entry: "/App.js",
